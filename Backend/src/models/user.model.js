e9d61513a2e262ee4f429952fd56eb9dc46b5f3e
Jagdish-Padhi <jagdishpadhi9@gmail.com>
2025-12-27 01:33:18 +0000

    feat : Add user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,1 +1,46 @@
-//userSchema
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        refreshToken: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
